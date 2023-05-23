ab676c63ac368d1a3d2bc4f9bf9115d23b17386e
Justin Fuller <justindanielfuller@gmail.com>
2023-05-23 13:27:24 -0400

    poem

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
+	"sync"
 )
 
 func main() {
@@ -26,6 +29,14 @@ func main() {
 			}
 		}
 
+		low := 0
+		high := len(split) - 1
+		for high > low {
+			split[low], split[high] = split[high], split[low]
+			low++
+			high--
+		}
+
 		type data struct {
 			Entries [][]byte
 		}
@@ -35,12 +46,76 @@ func main() {
 	})
 
 	http.HandleFunc("/poem", func(w http.ResponseWriter, r *http.Request) {
-		dir, err := os.ReadDir("./poem")
+		files, err := os.ReadDir("./poem")
 		if err != nil {
 			log.Fatalf("Error reading poetry entries: %s", err)
 		}
 
-		log.Print(dir)
+		var names []string
+		for _, dir := range files {
+			if name := dir.Name(); strings.HasSuffix(name, ".md") && !dir.IsDir() {
+				names = append(names, name)
+			}
+		}
+
+		sort.Slice(names, func(i, j int) bool {
+			s1 := strings.Split(names[i], ".")[0]
+			s2 := strings.Split(names[j], ".")[0]
+
+			n1, err := strconv.Atoi(s1)
+			if err != nil {
+				log.Fatalf("Error parsing string to int: %s", err)
+			}
+
+			n2, err := strconv.Atoi(s2)
+			if err != nil {
+				log.Fatalf("Error parsing string to int: %s", err)
+			}
+
+			return n2 > n1
+		})
+
+		contents := make([][]byte, len(names), len(names))
+		var wg sync.WaitGroup
+
+		wg.Add(len(names))
+
+		for i, name := range names {
+			i := i
+			name := name
+
+			go func() {
+				defer wg.Done()
+
+				file, err := os.ReadFile("./poem/" + name)
+				if err != nil {
+					log.Fatalf("Error reading file: %s", err)
+				}
+
+				content := file
+				content = bytes.Split(content, []byte("```"))[1]
+				contents[i] = content
+			}()
+		}
+
+		wg.Wait()
+
+		low := 0
+		high := len(contents) - 1
+
+		for high > low {
+			contents[low], contents[high] = contents[high], contents[low]
+			low++
+			high--
+		}
+
+		var poems = template.Must(template.ParseFiles("./poem/main.template.html"))
+		type data struct {
+			Entries [][]byte
+		}
+		poems.Execute(w, data{
+			Entries: contents,
+		})
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
