61dcceff871a24b63290b0ed135d3ef8abe74ae1
Justin Fuller <justindanielfuller@gmail.com>
2023-05-24 09:00:07 -0400

    Flash of unstyled text

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/justindfuller/justindfuller.com
 
 go 1.19
+
+require github.com/pkg/errors v0.9.1
+
+require golang.org/x/sync v0.2.0
--- main.go
+++ main.go
@@ -2,195 +2,229 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
-	"sync"
 	"text/template"
+
+	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
 )
+
+func aphorisms() ([][]byte, error) {
+	file, err := os.ReadFile("./aphorism/entries.txt")
+	if err != nil {
+		return nil, errors.Wrap(err, "error reading aphorisms entries.txt")
+	}
+
+	split := bytes.Split(file, []byte("\n"))
+	for i := len(split) - 1; i >= 0; i-- {
+		split[i] = bytes.TrimSpace(split[i])
+		if split[i] == nil {
+			split = append(split[:i], split[i+1:]...)
+		}
+	}
+
+	low := 0
+	high := len(split) - 1
+	for high > low {
+		split[low], split[high] = split[high], split[low]
+		low++
+		high--
+	}
+
+	return split, nil
+}
+
+func poems() ([][]byte, error) {
+	files, err := os.ReadDir("./poem")
+	if err != nil {
+		return nil, errors.Wrap(err, "error reading poetry entries")
+	}
+
+	var names []string
+	for _, dir := range files {
+		if name := dir.Name(); strings.HasSuffix(name, ".md") && !dir.IsDir() {
+			names = append(names, name)
+		}
+	}
+
+	sort.Slice(names, func(i, j int) bool {
+		s1 := strings.Split(names[i], ".")[0]
+		s2 := strings.Split(names[j], ".")[0]
+
+		n1, err := strconv.Atoi(s1)
+		if err != nil {
+			log.Fatalf("Error parsing string to int: %s", err)
+		}
+
+		n2, err := strconv.Atoi(s2)
+		if err != nil {
+			log.Fatalf("Error parsing string to int: %s", err)
+		}
+
+		return n2 > n1
+	})
+
+	contents := make([][]byte, len(names), len(names))
+	var wg errgroup.Group
+
+	for i, name := range names {
+		i := i
+		name := name
+
+		wg.Go(func() error {
+			path := fmt.Sprintf("./poem/%s", name)
+
+			file, err := os.ReadFile(path)
+			if err != nil {
+				return errors.Wrapf(err, "error reading file: %s", path)
+			}
+
+			content := file
+			content = bytes.Split(content, []byte("```"))[1]
+			contents[i] = content
+
+			return nil
+		})
+	}
+
+	if err := wg.Wait(); err != nil {
+		return nil, errors.Wrap(err, "Error reading poems")
+	}
+
+	low := 0
+	high := len(contents) - 1
+
+	for high > low {
+		contents[low], contents[high] = contents[high], contents[low]
+		low++
+		high--
+	}
+
+	return contents, nil
+}
+
+func stories() ([][]byte, error) {
+	files, err := os.ReadDir("./story")
+	if err != nil {
+		return nil, errors.Wrap(err, "error reading story directory")
+	}
+
+	var names []string
+	for _, dir := range files {
+		if name := dir.Name(); strings.HasSuffix(name, ".md") && !dir.IsDir() {
+			names = append(names, name)
+		}
+	}
+
+	contents := make([][]byte, len(names), len(names))
+	var wg errgroup.Group
+
+	for i, name := range names {
+		i := i
+		name := name
+
+		wg.Go(func() error {
+			path := fmt.Sprintf("./story/%s", name)
+
+			file, err := os.ReadFile(path)
+			if err != nil {
+				return errors.Wrapf(err, "error reading story: %s", path)
+			}
+
+			lines := bytes.Split(file, []byte("\n"))
+
+			for i := len(lines) - 1; i >= 0; i-- {
+				lines[i] = bytes.TrimSpace(lines[i])
+				if lines[i] == nil || bytes.Equal(lines[i], []byte("\n")) {
+					lines = append(lines[:i], lines[i+1:]...)
+				}
+			}
+
+			for i, line := range lines {
+				if bytes.HasPrefix(line, []byte("<h")) {
+					continue
+				}
+
+				if line == nil || bytes.Equal(line, []byte("\n")) {
+					continue
+				}
+
+				lines[i] = append([]byte("<p>"), line...)
+				lines[i] = append(lines[i], []byte("</p>")...)
+			}
+
+			contents[i] = bytes.Join(lines, nil)
+
+			return nil
+		})
+	}
+
+	if err := wg.Wait(); err != nil {
+		return nil, errors.Wrap(err, "error reading stories")
+	}
+
+	low := 0
+	high := len(contents) - 1
+
+	for high > low {
+		contents[low], contents[high] = contents[high], contents[low]
+		low++
+		high--
+	}
+
+	return contents, nil
+}
+
+type data struct {
+	Entries [][]byte
+}
 
 func main() {
 	http.HandleFunc("/aphorism", func(w http.ResponseWriter, r *http.Request) {
-		var aphorisms = template.Must(template.ParseFiles("./aphorism/main.template.html"))
-
-		file, err := os.ReadFile("./aphorism/entries.txt")
+		entries, err := aphorisms()
 		if err != nil {
-			log.Fatalf("Error reading aphorism entries: %s", err)
+			http.Error(w, "Error reading Aphorisms", http.StatusInternalServerError)
+			log.Printf("Error reading Aphorisms: %s", err)
+
+			return
 		}
 
-		split := bytes.Split(file, []byte("\n"))
-		for i := len(split) - 1; i >= 0; i-- {
-			split[i] = bytes.TrimSpace(split[i])
-			if split[i] == nil {
-				split = append(split[:i], split[i+1:]...)
-			}
-		}
-
-		low := 0
-		high := len(split) - 1
-		for high > low {
-			split[low], split[high] = split[high], split[low]
-			low++
-			high--
-		}
-
-		type data struct {
-			Entries [][]byte
-		}
-		aphorisms.Execute(w, data{
-			Entries: split,
+		template.Must(template.ParseFiles("./aphorism/main.template.html")).Execute(w, data{
+			Entries: entries,
 		})
 	})
 
 	http.HandleFunc("/poem", func(w http.ResponseWriter, r *http.Request) {
-		files, err := os.ReadDir("./poem")
+		entries, err := poems()
 		if err != nil {
-			log.Fatalf("Error reading poetry entries: %s", err)
+			http.Error(w, "Error reading poems.", http.StatusInternalServerError)
+			log.Printf("Error reading poems.")
+
+			return
 		}
 
-		var names []string
-		for _, dir := range files {
-			if name := dir.Name(); strings.HasSuffix(name, ".md") && !dir.IsDir() {
-				names = append(names, name)
-			}
-		}
-
-		sort.Slice(names, func(i, j int) bool {
-			s1 := strings.Split(names[i], ".")[0]
-			s2 := strings.Split(names[j], ".")[0]
-
-			n1, err := strconv.Atoi(s1)
-			if err != nil {
-				log.Fatalf("Error parsing string to int: %s", err)
-			}
-
-			n2, err := strconv.Atoi(s2)
-			if err != nil {
-				log.Fatalf("Error parsing string to int: %s", err)
-			}
-
-			return n2 > n1
-		})
-
-		contents := make([][]byte, len(names), len(names))
-		var wg sync.WaitGroup
-
-		wg.Add(len(names))
-
-		for i, name := range names {
-			i := i
-			name := name
-
-			go func() {
-				defer wg.Done()
-
-				file, err := os.ReadFile("./poem/" + name)
-				if err != nil {
-					log.Fatalf("Error reading file: %s", err)
-				}
-
-				content := file
-				content = bytes.Split(content, []byte("```"))[1]
-				contents[i] = content
-			}()
-		}
-
-		wg.Wait()
-
-		low := 0
-		high := len(contents) - 1
-
-		for high > low {
-			contents[low], contents[high] = contents[high], contents[low]
-			low++
-			high--
-		}
-
-		var poems = template.Must(template.ParseFiles("./poem/main.template.html"))
-		type data struct {
-			Entries [][]byte
-		}
-		poems.Execute(w, data{
-			Entries: contents,
+		template.Must(template.ParseFiles("./poem/main.template.html")).Execute(w, data{
+			Entries: entries,
 		})
 	})
 
 	http.HandleFunc("/story", func(w http.ResponseWriter, r *http.Request) {
-		files, err := os.ReadDir("./story")
+		entries, err := stories()
 		if err != nil {
-			log.Fatalf("Error reading story entries: %s", err)
+			http.Error(w, "Error reading stories.", http.StatusInternalServerError)
+			log.Printf("Error reading stories: %s", err)
+
+			return
 		}
 
-		var names []string
-		for _, dir := range files {
-			if name := dir.Name(); strings.HasSuffix(name, ".md") && !dir.IsDir() {
-				names = append(names, name)
-			}
-		}
-
-		contents := make([][]byte, len(names), len(names))
-		var wg sync.WaitGroup
-
-		wg.Add(len(names))
-
-		for i, name := range names {
-			i := i
-			name := name
-
-			go func() {
-				defer wg.Done()
-
-				file, err := os.ReadFile("./story/" + name)
-				if err != nil {
-					log.Fatalf("Error reading file: %s", err)
-				}
-
-				lines := bytes.Split(file, []byte("\n"))
-
-				for i := len(lines) - 1; i >= 0; i-- {
-					lines[i] = bytes.TrimSpace(lines[i])
-					if lines[i] == nil || bytes.Equal(lines[i], []byte("\n")) {
-						lines = append(lines[:i], lines[i+1:]...)
-					}
-				}
-
-				for i, line := range lines {
-					if bytes.HasPrefix(line, []byte("<h")) {
-						continue
-					}
-
-					if line == nil || bytes.Equal(line, []byte("\n")) {
-						continue
-					}
-
-					lines[i] = append([]byte("<p>"), line...)
-					lines[i] = append(lines[i], []byte("</p>")...)
-				}
-
-				contents[i] = bytes.Join(lines, nil)
-			}()
-		}
-
-		wg.Wait()
-
-		low := 0
-		high := len(contents) - 1
-
-		for high > low {
-			contents[low], contents[high] = contents[high], contents[low]
-			low++
-			high--
-		}
-
-		var stories = template.Must(template.ParseFiles("./story/main.template.html"))
-		type data struct {
-			Entries [][]byte
-		}
-		stories.Execute(w, data{
-			Entries: contents,
+		template.Must(template.ParseFiles("./story/main.template.html")).Execute(w, data{
+			Entries: entries,
 		})
 	})
 
