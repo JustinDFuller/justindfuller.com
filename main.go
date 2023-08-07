763382a023c307f527b70437a48ad0a01c5a4cfa
Justin Fuller <justindanielfuller@gmail.com>
2023-08-07 09:03:10 -0400

    Grass

--- main.go
+++ main.go
@@ -91,6 +91,10 @@ func main() {
 		})
 	})
 
+	http.HandleFunc("/grass/service-worker.js", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./make/grass-sevice-worker.js")
+	})
+
 	http.HandleFunc("/grass", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./make/grass.html")
 	})
---
+++ make/grass-service-worker.js
@@ -0,0 +1,1 @@
+self.addEventListener('install', event => console.log('ServiceWorker installed'));
