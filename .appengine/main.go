ff90180242545126b69b9bf9d385d7457d2657a9
Justin Fuller <justindanielfuller@gmail.com>
2023-09-04 09:13:11 -0400

    redirect

--- .appengine/main.go
+++ .appengine/main.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/2022/01/living-on-24-hours-a-day", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/review/living-on-24-hours-a-day", http.StatusMovedPermanently)
+	})
+
 	http.Handle("/", http.NotFoundHandler())
 	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
 }
