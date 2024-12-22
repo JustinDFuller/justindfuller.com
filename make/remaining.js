9dbd257ec41a8f9ce877b21a6d4fc3753f9b25e8
Justin Fuller <justindanielfuller@gmail.com>
2024-12-22 13:10:11 +0000

    Fixes

--- make/remaining.js
+++ make/remaining.js
@@ -13,25 +13,29 @@ function handleSubmit(event) {
 }
 
 function process(birthdate) {
-    const birth = new Date(birthdate);
-    const now = new Date();
-    const diff = now.getTime() - birth.getTime();
-    const age = Math.round(diff / (1000 * 60 * 60 * 24 * 7));
-    const expectedWeeks = 79 * 52;
-    const expected = expectedWeeks - age
-
-    if (expected <= 0) {
+    try {
+        const birth = new Date(birthdate);
+        const now = new Date();
+        const diff = now.getTime() - birth.getTime();
+        const age = Math.round(diff / (1000 * 60 * 60 * 24 * 7));
+        const expectedWeeks = 79 * 52;
+        const expected = expectedWeeks - age
+    
+        if (expected <= 0) {
+            throw new Error("Expected is less than zero");
+        }
+    
+        document.getElementById("age").innerText = age;
+        document.getElementById("remaining").innerText = expected;
+        document.getElementById("percent").innerText = Math.round(((expectedWeeks - age) / expectedWeeks) * 100);
+        document.getElementById("expected-results").style.display = "block";
+        document.getElementById("unexpected-results").style.display = "none";
+    } catch (e) {
         document.getElementById("unexpected-results").style.display = "block";
         document.getElementById("expected-results").style.display = "none";
 
         return;
     }
-
-    document.getElementById("age").innerText = age;
-    document.getElementById("remaining").innerText = expected;
-    document.getElementById("percent").innerText = Math.round(((expectedWeeks - age) / expectedWeeks) * 100);
-    document.getElementById("expected-results").style.display = "block";
-    document.getElementById("unexpected-results").style.display = "none";
 }
 
 try {
